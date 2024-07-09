08e7be3f863a39b6f17c1dd01d9fa37a68c1e09e
Anna Bavaresco <74197386+annabavaresco@users.noreply.github.com>
2024-07-09 15:16:38 +0200

    updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ extensions = [
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 # exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'tutorial_notebooks/tutorial*/*_empty.ipynb']
-exclude_patterns = ['.DS_Store']
+exclude_patterns = ['.DS_Store', 'build']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -74,7 +74,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', 'build']
+# html_static_path = ['_static']
 master_doc = 'index'
 
 highlight_language = 'python3'
